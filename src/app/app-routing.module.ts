0d961c5208ae4235c898c57c61ba2c59d1386449
UTMIan <UTM20040084@utma.edu.mx>
2022-07-14 20:46:02 -0500

    Login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  }
+  {path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+  {path: 'register',loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
 @NgModule({
